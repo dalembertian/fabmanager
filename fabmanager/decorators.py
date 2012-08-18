b8612a87e5beb4e7d465fac5d44f5013bb2a54b9
Rubens Altimari <rubens@introspection.com.br>
2012-08-18 13:21:46 -0300

    Renaming decorators to start with "_" and avoid being listed in fab -l

--- fabmanager/decorators.py
+++ fabmanager/decorators.py
@@ -5,35 +5,37 @@ from fabric.api import *
 
 # These variables must be defined at the actual fabfile.py
 # Address of the proxy server intermediating the executation
-env.proxy_server = ''
+#env.proxy_server = ''
 
 # Location, at the proxy server, where fabfily.py will reside
-env.proxy_home = ''
+#env.proxy_home = ''
 
 # List of hosts and/or roles specified for this session - will
 # be included in the fab command executed at the proxy server
-env.proxy_hosts = []
-env.proxy_roles = []
+#env.proxy_hosts = []
+#env.proxy_roles = []
 
-def is_running_on_proxy():
+def _is_running_on_proxy():
     """
     Returns True if this fabfile is being run on the proxy server.
     """
     return env.real_fabfile.find(env.proxy_home) < 0
 
-def run_on_proxy(role=None, host=None):
+def _run_on_proxy(role=None, host=None):
     """
-    Decorator that creates the actual decorator! This is necessary in order to
-    be able to pass parameters to the actual decorator. Usage:
+    Decorator that creates the actual decorator to route tasks through proxy.
+    This is necessary in order to be able to pass parameters to the actual decorator.
+
+    Usage:
 
         @hosts(env.proxy_server)
-        @run_on_proxy([host='somehost'|role='somerole'])
+        @_run_on_proxy([host='somehost'|role='somerole'])
         def mytask():
 
     Each task must be surrounded by a @hosts decorator specifying the proxy server,
     so the task will be initially run at the proxy.
 
-    Then the @run_on_proxy decorator can be used with or without specifying the actual
+    Then the @_run_on_proxy decorator can be used with or without specifying the actual
     servers where the task should be run. If no servers are specifyied, then the lists
     env.proxy_roles or env.proxy_hosts should be previously populated by some other task.
     """
@@ -47,7 +49,7 @@ def run_on_proxy(role=None, host=None):
             If it is, invokes fab again specifying some other server.
             If it is already being run on some other server, just execute the task.
             """
-            if is_running_on_proxy():
+            if _is_running_on_proxy():
                 # There are several ways to specify in which other server this
                 # task is to be run. Hosts/roles specified by the decorator itself
                 # have higher priority.
@@ -70,10 +72,9 @@ def run_on_proxy(role=None, host=None):
                     if kwargs:
                         arguments.append(','.join(['%s=%s' % (k,v) for k,v in kwargs.items()]))
                     if args or kwargs:
-                        command = 'fab %s:%s' % (task.__name__, ','.join(arguments))
+                        run('fab %s:%s' % (task.__name__, ','.join(arguments)))
                     else:
-                        command = 'fab %s' % task.__name__
-                    run(command)
+                        run('fab %s' % task.__name__)
             else:
                 task(*args, **kwargs)
 
