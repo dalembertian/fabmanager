597f64ae56bc18dd2efb1ca53ccf036109bfd6b7
Rubens Altimari <rubens@introspection.com.br>
2025-10-30 15:11:51 +0100

    Moving from setuptools to hatchling

--- fabmanager/__init__.py
+++ fabmanager/__init__.py
@@ -1,1 +1,18 @@
-__version__ = "1.5"
+# encoding: utf-8
+"""
+Installing:
+    pip install .
+    pip install [-e] .
+
+Building (and installing) for distribution:
+    pip install build
+    python -m build
+    pip install dist/*.whl
+
+Testing:
+    python -c "import fabmanager; print('ok')"
+
+Publish to PyPI:
+    pip install twine
+    twine upload dist/*
+"""
